3fd110b0d185d0a98817108701fe0a58dfbe934e
gustavo.rodrigues <gustavo.rodrigues@hubess.com.br>
2021-11-18 19:35:40 -0300

    estilizando search

--- components/layout/header/search/index.js
+++ components/layout/header/search/index.js
@@ -1,5 +1,11 @@
+import Image from "next/image";
+
 export default function Search() {
   return<>
-    <p>Search</p>
+    <div className="search-contianer">
+      <span className="placeholder paragraph pink-jussi">Buscar</span>
+      <input className="paragraph"/>
+      <Image src="/icons/search.svg" alt="search" height={24} width={24}/>
+    </div>
   </>;
 }
