595a5089f0856e3524242397bcc514f5d91139a6
gustavo.rodrigues <gustavo.rodrigues@hubess.com.br>
2021-11-18 21:03:53 -0300

    criando e estilizando footer

---
+++ mock/layout/social.js
@@ -0,0 +1,17 @@
+export const socialData = [
+  {
+    id: 1,
+    imageSource: "/icons/facebook.png",
+    link: "https://www.facebook.com/agencia.jussi/",
+  },
+  {
+    id: 2,
+    imageSource: "/icons/instagram.png",
+    link: "https://www.instagram.com/jussi/",
+  },
+  {
+    id: 3,
+    imageSource: "/icons/linkedin.png",
+    link: "https://www.linkedin.com/company/agencia-jussi/",
+  },
+]
