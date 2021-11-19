ac830c6373b85aebe4a99b7c78645d5623ae004e
gustavo.rodrigues <gustavo.rodrigues@hubess.com.br>
2021-11-18 21:26:16 -0300

    adicionando imagens e scss

--- components/homePage/logoBar/index.js
+++ components/homePage/logoBar/index.js
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function LogoBar() {
   return<>
     <section id="logo-bar">
       <span className="paragraph">Nossas principais lojas VTEX</span>
+      <span>&#8594;</span>
+      <Image src="/images/homePage/logoBar/logo-brastemp.png" alt="logo-brastemp" height={24} width={156} />
+      <Image src="/images/homePage/logoBar/logo-compracerta.png" alt="logo-compracerta" height={21} width={180} />
+      <Image src="/images/homePage/logoBar/logo-consul.png" alt="logo-consul" height={38} width={88} />
+      <Image src="/images/homePage/logoBar/logo-thebar.png" alt="logo-thebar" height={38} width={130} />
     </section>
   </>;
 }
