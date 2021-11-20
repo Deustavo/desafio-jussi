4e379fd52e7ce400005d91ece062fda5ee8070ec
gustavo.rodrigues <gustavo.rodrigues@hubess.com.br>
2021-11-20 16:04:26 -0300

    adicionando componente de loading

--- components/layout/header/search/index.js
+++ components/layout/header/search/index.js
@@ -3,11 +3,12 @@ import { useRouter } from "next/router";
 import Image from "next/image";
 import Link from "next/link";
 
+import Loading from "../../../shared/loading";
 import { SearchContext } from "../../../../provider/search";
 import api from "../../../../services/api";
 
 export default function Search() {
-  const {setSearch} = React.useContext(SearchContext);
+  const {setSearch, loading, setLoading} = React.useContext(SearchContext);
   const [text, setText] = useState("");
   const router = useRouter();
 
@@ -20,7 +21,10 @@ export default function Search() {
     setText(search);
 
     if (search !== "") {
+      setLoading(true);
       let get = await api.get(`people/?search=${search}`);
+      setLoading(false);
+
       response = get.data.results;
     }
 
@@ -33,11 +37,13 @@ export default function Search() {
   return<>
     <section className="search-contianer" htmlFor="header-search">
       <input className="paragraph" id="header-search" value={text} onChange={(e) => setTextData(e.target.value)} placeholder="Buscar"/>
-      <Link href={"/search"}>
-        <a className="link-icon">
-          <Image src="/icons/search.svg" alt="search" height={24} width={24}/>
-        </a>
-      </Link>
+      { loading ? <Loading />:
+        <Link href={"/search"}>
+          <a className="link-icon">
+            <Image src="/icons/search.svg" alt="search" height={24} width={24}/>
+          </a>
+        </Link>
+      }
       <div className="dinamic-border-input"/>
     </section>
   </>;
